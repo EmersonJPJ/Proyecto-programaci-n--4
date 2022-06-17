9ce950b5267f5de2866086582f592ca98b363834
EmersonJPJ <emerson04vade@gmail.com>
2022-06-17 11:30:56 -0600

    Avance 1
    Comentarios en el codigo

---
+++ Avance01-Proyecto/src/main/java/com/sc202/avance01/proyecto/Avance01Proyecto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc202.avance01.proyecto;
+
+/**
+ *
+ * @author Jeausthin
+ */
+public class Avance01Proyecto {
+
+    public static void main(String[] args) {
+        //Se desarrollaron las public class necesarias para el desarrollo del proyecto, un ciclo
+        //while con estructuras de desicion para activar el programa de vacunacion 
+    }
+}
---
+++ Avance01-Proyecto/src/main/java/com/sc202/avance01/proyecto/Menu.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sc202.avance01.proyecto;
+
+/**
+ *
+ * @author Jeausthin
+ */
+public class Menu
+{
+    public void menu()
+    {
+        boolean salir = false;
+        
+        //Si salir es igual a false abandona el bucle while
+        while (salir == false)
+        {    //Declarar variable en 0 para iniciar el bucle escogiendo un numero
+            int opcion = 0;
+            
+            
+            //Estructura de desicion para continuar con el bucle
+            if (opcion == 0)
+            {
+                break;
+            }
+        }
+                
+    }
+}
